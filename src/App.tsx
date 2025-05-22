2ca92ac2be6977b097fbae32ee9550d0b5d2352a
ratchaluk_gl <man_tep@hotmail.com>
2025-05-22 15:28:19 +0700

    SPY1990-6-Test-Move-To-Progress-Complete

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function App() {
 
       <p>MAM Add Spy</p>
 
+      <p>Man Test Move to Progress.</p>
       <div className="card">
         <button>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
