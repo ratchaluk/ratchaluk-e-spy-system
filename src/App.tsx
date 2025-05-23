2c89c3f72a4df764bc4d4cf310f8aee8943c5880
ratchaluk_gl <man_tep@hotmail.com>
2025-05-23 10:42:28 +0700

    test error

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p className='read-the-docs'>
+      <p className='read-the-docs'>>
         Bugs Here.
       </p>
     </>
